81b50d11cb3df9cfdfecb20c8d9b1db4ceafe1fe
Matthew Hansen <MY_NAME@example.comlearntocode711@gmail.comlscleargit config --global user.email MY_NAME@example.comlearntocode711@gmail.comclear>
2020-10-24 14:00:31 +1000

    ABC OPTICAL

---
+++ JavaScript/footer.js
@@ -0,0 +1,9 @@
+document.write(`
+    <footer>
+    <article>
+        <p>&#169; 2020 ABC Optical</p>
+        <p>PH 07 3256 7101</p>
+        <p>Myer Centre in the Queen Street Mall on Level 1, Shop number 36</p>
+      </article>
+    </footer>
+`);
---
+++ JavaScript/headnav.js
@@ -0,0 +1,26 @@
+document.write(`
+
+    <header><label for="check" id"hamIcon" >&#9776;</label
+    ><input type="checkbox" name="check" id="check" />
+    <nav id="ham">
+      <ul>
+    <li><a href="/index.html">Home</a></li>
+    <li><a href="/Pages/booking.html">Bookings</a></li>
+    <li><a href="/Pages/products.html">Products</a></li>
+    <li><a href="/Pages/contact.html">Contact</a></li>
+    <li><a href="/Pages/blog.html">Blog</a></li>
+    </ul>
+    </nav></header>
+    
+
+    <nav id="navbar">
+    <ul>
+    <li><a href="/index.html" class="animation">Home</a></li>
+    <li><a href="/Pages/booking.html" class="animation">Bookings</a></li>
+    <li><a href="/Pages/products.html" class="animation">Products</a></li>
+    <li><a href="/Pages/contact.html" class="animation">Contact</a></li>
+    <li><a href="/Pages/blog.html" class="animation">Blog</a></li>
+    </ul>
+    </nav>
+
+`);
